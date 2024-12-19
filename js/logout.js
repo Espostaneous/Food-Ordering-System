9bf83a236d79c97e54ffd48996f1f662c2bb4eaa
aech <mijaresgiancyril@gmail.com>
2024-12-20 01:05:02 +0800

    almighty push!

--- js/logout.js
+++
@@ -1,7 +0,0 @@
-function confirmLogout() {
-    // Prompt the user for confirmation
-    if (confirm("Do you want to log out?")) {
-        // If user confirms, redirect to logout PHP script
-        window.location.href = "client/logout.php?logout=true";
-    }
-}
