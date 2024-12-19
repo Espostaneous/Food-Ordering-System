7af2c992f5e8f747c69efba3c1a33e7e3e137528
Espostaneous <ralstone2010@gmail.com>
2024-12-19 19:33:08 +0800

    Update listepanier.php
    Modified checkout button with pop-up functionality

--- listepanier.php
+++ listepanier.php
@@ -232,9 +232,11 @@ $data = $res->fetch();
                             border-color: #034694;
                         }
                     </style>
-                    <a href="checkout.php" class="btn btn-success">
+                    <a href="javascript:void(0)" class="btn btn-success" data-toggle="modal"
+                        data-target="#checkoutModal">
                         Checkout <span class="glyphicon glyphicon-play"></span>
                     </a>
+
                 </div>
             </div>
         </div>
@@ -256,6 +258,28 @@ $data = $res->fetch();
 
         <!-- Main JS -->
         <script src="js/app.min.js "></script>
+        <!-- Checkout Confirmation Modal -->
+        <div class="modal fade" id="checkoutModal" tabindex="-1" role="dialog" aria-labelledby="checkoutModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="checkoutModalLabel">Confirm Checkout</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Are you sure you want to proceed to checkout?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <a href="checkout.php" class="btn btn-success">Yes, Proceed</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </body>
 
 </html>
